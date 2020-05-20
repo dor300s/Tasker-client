ff7e82c6036ceb7d2a8abe6e0eb546278621b7a1
eliyahu-dayan-1 <62394293+eliyahu-dayan-1@users.noreply.github.com>
2020-05-20 17:44:06 +0300

    eliyahu add Board data

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { getUsers } from '../tempSeviceData/tempUserData.js';
+import { getBoards } from '../tempSeviceData/tempBoardData.js';
 
 let gUsers = getUsers();
 console.log(gUsers)
+console.log(getBoards())
 
 
 export default class UserDetails extends React.Component {
