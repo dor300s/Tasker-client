e4510f3b5bf51fd80a8605c25ca5869a33ba0b78
reemnx <reemnx@gmail.com>
2020-05-21 21:07:38 +0300

    reem

--- src/cmps/NavBar.jsx
+++ src/cmps/NavBar.jsx
@@ -15,18 +15,12 @@ class NavBar extends React.Component {
     }
 
     componentDidMount() {
-        console.log(this.props.boards);
         // this.getLoggedUserDetails() // only at production ENV 
         this.props.setBoards()
         boardService.query()
     }
 
     getLoggedUserDetails = () => {
-<<<<<<< HEAD
-=======
-        //TOREMOVE
-        if(!this.props.loggedUser) return
->>>>>>> 7943b5be460e071d0e76a92411e1fe7200272196
         userService.get(this.props.loggedUser._id)
             .then(res => this.setState({ loggedUser: res }))
     }
